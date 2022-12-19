097ede8d4c2482937dd597d9dea41c995009fbff
SkaLe3 <39skale@gmail.com>
2022-12-19 22:51:11 +0200

    nothing

--- Engine/src/Engine/Core/EntryPoint.h
+++ Engine/src/Engine/Core/EntryPoint.h
@@ -1,12 +1,14 @@
 #pragma once
 
 #include "Application.h"
+#include <iostream>
 
 extern Engine::Application* Engine::CreateApplication();
 
 int main()
 {
 	auto app = Engine::CreateApplication();
+
 	app->Run();
 	delete app;
 	return 0;
