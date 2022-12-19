e8ba506deee9fee8f5dac2b43b5a2470c626386a
SkaLe3 <39skale@gmail.com>
2022-12-19 16:37:56 +0200

    added WindowResizeEvent

--- Engine/src/Engine/Events/ApplicationEvent.h
+++ Engine/src/Engine/Events/ApplicationEvent.h
@@ -12,4 +12,20 @@ namespace Engine {
 		static EventType GetStaticType() { return EventType::WindowClose; }
 		virtual EventType GetEventType() const override { return GetStaticType(); }
 	};
+
+	class ENGINE_API WindowResizeEvent : public Event
+	{
+	public:
+		WindowResizeEvent(unsigned int width, unsigned int height)
+			: m_Width(width), m_Height(height) {}
+
+		unsigned int GetWidth() const { return m_Width; }
+		unsigned int GetHeight() const { return m_Height; }
+
+		static EventType GetStaticType() { return EventType::WindowResize; }
+		virtual EventType GetEventType() const override { return GetStaticType(); }
+
+	private:
+		unsigned int m_Width, m_Height;
+	};
 }
