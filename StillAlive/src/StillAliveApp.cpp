980084f3a7c15df7b4c756dc42d7e50d92d270db
SkaLe3 <39skale@gmail.com>
2022-12-20 15:17:20 +0200

    example

--- StillAlive/src/StillAliveApp.cpp
+++ StillAlive/src/StillAliveApp.cpp
@@ -13,12 +13,25 @@ class Example : public Engine::Layer
 		std::cout << typeid(event.GetEventType()).name() << std::endl;
 	}
 };
+
+class Example1 : public Engine::Layer
+{
+	void OnUpdate() override
+	{
+		std::cout << "----------" << std::endl;
+	}
+	void OnEvent(Engine::Event& event) override
+	{
+		std::cout << typeid(event.GetEventType()).name() << std::endl;
+	}
+};
 class StillAliveApp : public  Engine::Application
 {
 public:
 	StillAliveApp() :  Engine::Application() 
 	{
 		PushLayer(new Example());
+		PushLayer(new Example1());
 	}
 	~StillAliveApp()
 	{
