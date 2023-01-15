745455122632d0e89e8f68c86bf1152ce537f684
SkaLe3 <39skale@gmail.com>
2023-01-15 19:32:50 +0200

    refactoring

---
+++ premake5.lua
@@ -0,0 +1,130 @@
+workspace "StillAlive"
+	architecture "x64"
+	startproject "StillAlive"
+
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+-- Include directories relative to root folder (solution directory)
+IncludeDir = {}
+--IncludeDir["GLFW"] = "Engine/vendor/GLFW/include"
+--IncludeDir["Glad"] = "Engine/vendor/Glad/include"
+IncludeDir["glm"] = "Engine/vendor/glm"
+
+group "Dependencies"
+--	include "Engine/vendor/GLFW"
+--	include "Engine/vendor/Glad"
+
+group ""
+
+project "Engine"
+	location "Engine"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	pchheader "pch.h"
+	pchsource "Engine/src/pch.cpp"
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/vendor/glm/glm/**.hpp",
+		"%{prj.name}/vendor/glm/glm/**.inl",
+	}
+
+	defines
+	{
+		"_CRT_SECURE_NO_WARNINGS"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+--		"%{IncludeDir.GLFW}",
+--		"%{IncludeDir.Glad}",
+		"%{IncludeDir.glm}"
+	}
+
+	links 
+	{ 
+		"GLFW",
+		"Glad",
+		"opengl32.lib"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+		defines
+		{
+			"GLFW_INCLUDE_NONE"
+		}
+
+	filter "configurations:Debug"
+		defines "EG_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "EG_RELEASE"
+		runtime "Release"
+		optimize "on"
+
+
+project "StillAlive"
+	location "StillAlive"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "on"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/Creatures/**.h",
+		"%{prj.name}/src/Weapon/**.h",
+		"%{prj.name}/src/**.cpp",
+		"%{prj.name}/src/Creatures/**.cpp",
+		"%{prj.name}/src/Weapon/**.cpp"
+	}
+
+	includedirs
+	{
+		"Engine/src",
+		"Engine/vendor",
+		"%{IncludeDir.glm}"
+	}
+
+	links
+	{
+		"Engine"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+
+	filter "configurations:Debug"
+		defines "EG_DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "EG_RELEASE"
+		runtime "Release"
+		optimize "on"
+
